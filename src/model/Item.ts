e15581d19b2c5393d3fc94f743087515ed12b88c
harry0615 <harry0615.lee@gmail.com>
2017-08-01 16:07:53 +0900

    modify item model

--- src/model/Item.ts
+++ src/model/Item.ts
@@ -2,12 +2,12 @@ export interface Item {
   id: number;
   title: string;
   url: string;
-  by: string;
+  //by: string;
   time: number;
-  score: number;
-  text?: string;
-  descendants?: number;
-  kids?: number[];
+  //score: number;
+  //text?: string;
+  //descendants?: number;
+  //kids?: number[];
   isFavorite?: boolean;
 }
 
